48afdc00b45c1cf38410ce0bcb8734a98569fd92
Yash <139779840+yashop7@users.noreply.github.com>
2024-12-31 01:07:05 +0530

    Refactor authentication logic by commenting out unused code and simplifying layout component

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,12 +1,9 @@
 "use client"
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "@repo/ui/appbar";
 
-export default function Page(): JSX.Element {
-  const session = useSession();
+export default function Page() {
   return (
    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+    Hello
    </div>
   );
 }
