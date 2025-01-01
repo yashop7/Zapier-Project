692ca6479e5b3b72a0c02404fc4bd1a000e2df3c
Yash <139779840+yashop7@users.noreply.github.com>
2025-01-02 02:50:48 +0530

    Refactor authentication forms to include name field, integrate axios for API calls, and implement validation schemas for signup and signin

---
+++ apps/frontend/src/app/config.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+export const BACKEND_URL = "http://localhost:3001";
+
+export const Signupschema = z.object({
+    name : z.string().min(3),
+    email: z.string().min(5),
+    password: z.string().min(3),
+});
+
+export const SigninSchema = z.object({
+    name : z.string(), 
+    email: z.string(),
+    password: z.string()
+});
--- apps/frontend/src/components/signup-form.tsx
+++ apps/frontend/src/components/signup-form.tsx
@@ -1,44 +1,107 @@
+"use client";
+
+import axios from "axios";
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useEffect, useState } from "react";
+import { BACKEND_URL, Signupschema } from "@/app/config";
+import { useRouter } from "next/navigation";
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"form">) {
+    const [name , setName] = useState('');
+    const [password , setPassword] = useState('');
+    const [email , setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+      if(error){
+        setTimeout(() => {
+          setError("");
+        }, 3000);
+      }
+    }, [error, loading]);
+  
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">Signup your account</h1>
         <p className="text-balance text-sm text-muted-foreground">
-          Enter your email below to login to your account
+          Enter your Credentials to create an account
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-2">
           <Label htmlFor="name">Name</Label>
-          <Input id="name" type="text" placeholder="Enter your name" required/>
+          <Input id="name" type="text" placeholder="Enter your name" required
+          onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input id="email" type="email" placeholder="m@example.com" required 
+          onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
           </div>
-          <Input id="password" type="password" required />
+          <Input id="password" type="password" required 
+          onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <Button type="submit" className="w-full">
-          Login
+        <div className="text-red-500 text-xs text-center">{error}</div>
+        <Button type="submit" className="w-full"
+        onClick={async (e) => {
+          try{
+            e.preventDefault();
+            setLoading(true);
+            const body = {
+              name: name,
+              email: email,
+              password: password
+            }
+            const parsedBody = Signupschema.safeParse(body);
+            if(!parsedBody.success){
+              setError("Invalid data");
+              setLoading(false);
+              return;
+            }
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/signup`, body)
+            console.log("response: ", response.status === 200);
+            
+            router.push("/"); //Redirect to home page
+            
+            console.log("response: ", response.data.message);
+            setLoading(false);
+          }
+          catch(err){
+            // setError(err.response.data.message);
+            console.log("err: ", err);
+
+            setLoading(false);
+          }
+        }}
+        >
+          
+          {loading ? 
+          <div role="status">
+          <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span className="sr-only">Loading...</span>
+      </div>
+      
+          : "Sign Up"}
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-background px-2 text-muted-foreground">
